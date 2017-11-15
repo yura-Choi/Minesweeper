032be089a006f352ddf85242e5ce9af43d75103b
yura-Choi <dbfk1207@gmail.com>
2017-11-16 02:36:42 +0900

    Initial commit

---
+++ unity/Minesweeper/Assets/Scripts/AudioController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioController : MonoBehaviour {
+    
+	void Update () {
+        string status_BGM = PlayerPrefs.GetString("bgm");
+
+        // If status_BGM is true, the AudioListener will be listening.
+        switch (status_BGM.ToLower()) {
+        case "false":
+            AudioListener.pause = true;
+            break;
+        case "true": default:
+            AudioListener.pause = false;
+            break;
+        }
+	}
+}
