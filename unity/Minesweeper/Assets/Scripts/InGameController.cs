3090cc6489e409e8f600a8377f75d5a66659eb33
yura-Choi <dbfk1207@gmail.com>
2017-11-18 02:43:55 +0900

    Add menu to select level

---
+++ unity/Minesweeper/Assets/Scripts/InGameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameController : MonoBehaviour {
+
+    public GameObject panel_SelectLevel;
+    public GameObject panel_GameScreen;
+
+    public GameObject prefeb_block_text;
+
+    private string level = "";
+
+	void Start () {
+        panel_SelectLevel.SetActive(true);
+        panel_GameScreen.SetActive(false);
+	}
+	
+	void Update () {
+		
+	}
+
+    // select level menu
+	public void level_EASY() {
+		this.level = "easy";
+        panel_SelectLevel.SetActive(false);
+        panel_GameScreen.SetActive(true);
+	}
+    public void level_NORMAL() {
+        this.level = "normal";
+        panel_SelectLevel.SetActive(false);
+        panel_GameScreen.SetActive(true);
+    }
+    public void level_HARD() {
+        this.level = "hard";
+        panel_SelectLevel.SetActive(false);
+        panel_GameScreen.SetActive(true);
+    }
+
+    public void back_TITLE() {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void map_block_open() {
+        prefeb_block_text.SetActive(true);
+    }
+
+}
