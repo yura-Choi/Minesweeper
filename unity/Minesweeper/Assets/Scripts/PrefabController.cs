080793e2dcb6af562cdb1175ab91d2b2731a837b
yura-Choi <dbfk1207@gmail.com>
2017-11-22 02:38:24 +0900

    Modify PrefabController script to calculate surronding mines

--- unity/Minesweeper/Assets/Scripts/PrefabController.cs
+++ unity/Minesweeper/Assets/Scripts/PrefabController.cs
@@ -8,33 +8,65 @@ public class PrefabController : MonoBehaviour {
 
     public int[,] array_map;
 
-    private void Start()
-    {
+    private void Start() {
         this.array_map = CreateMap();
-
     }
 
     private int WhereAreYou(int[,] map, int index) {
-        if (index == map.Length) return 6;
-        else if (index == map.GetLength(0)) return 1;
+        if (index == (map.Length - 1)) return 6;
+        else if (index == 0) return 1;
         else return 3;
     }
 
-    private int CalculateMinesCount(int[,] map, int i, int j) {
+    private int CountMines(int[,] map, int i, int i_state, int i_end, int j, int j_state, int j_end) {
+        int count = 0;
+
+        for (int k = i_state; k < i_end; k++) {
+            for (int l = j_state; l < j_end; l++) {
+                if (k == 0 && l == 0) continue;
+                if (map[i + k, j + 1] == 9) count++;
+            }
+        }
+
+        return count;
+    }
+
+    private int CalculateSurroundingMines(int[,] map, int i, int j) {
         int count = 0;
 
         switch (WhereAreYou(map, i) + WhereAreYou(map, j)) {
             case 12: // both indexes are MAX index.
+                count = CountMines(map, i, -1, 1, j, -1, 1);
                 break;
             case 9:  // one is MAX, the other is rest number.
+                if (i > j) {
+                    count = CountMines(map, i, -1, 1, j, -1, 2);
+                    break;
+                }
+                count = CountMines(map, i, -1, 2, j, -1, 1);
                 break;
             case 7:  // one is zero, the other is MAX.
+                if (i > j) {
+                    count = CountMines(map, i, -1, 1, j, 0, 2);
+                    break;
+                }
+                count = CountMines(map, i, 0, 2, j, -1, 1);
                 break;
             case 6:  // both indexes are rest number.
+                count = CountMines(map, i, -1, 2, j, -1, 2);
                 break;
             case 4:  // one is zero, the other is rest number.
+                if (i > j) {
+                    count = CountMines(map, i, -1, 1, j, 0, 2);
+                    break;
+                }
+                count = CountMines(map, i, -1, 1, j, -1, 2);
                 break;
             case 2:  // both indexes are zero.
+                count = CountMines(map, i, -1, 1, j, 0, 2);
+                break;
+            default:
+                Debug.Log("InGame: failed to calculate surrounding mines");
                 break;
         }
 
@@ -42,22 +74,22 @@ public class PrefabController : MonoBehaviour {
     }
 
     private int[,] CreateMap() {
-		int width = 0, height = 0, minesweeper = 0;
+		int width = 0, height = 0, count_mines = 0;
 		int count_assigned = 0;
 
 		switch (PlayerPrefs.GetString("level").ToLower())
 		{
 			case "easy":
 				width = 5; height = 8;
-				minesweeper = 10;
+				count_mines = 10;
 				break;
 			case "normal":
 				width = 6; height = 9;
-				minesweeper = 20;
+				count_mines = 20;
 				break;
 			case "hard":
 				width = 7; height = 10;
-				minesweeper = 30;
+				count_mines = 30;
 				break;
 			default:
 				Debug.Log("InGame: undefined stage level");
@@ -67,25 +99,36 @@ public class PrefabController : MonoBehaviour {
 		int[,] mineMap = new int[width, height];
 		System.Random r = new System.Random();
 
+        // Assign mines.
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
                 // All mines were assigned to the 'mineMap'.
-				if (count_assigned == minesweeper) break;
+				if (count_assigned == count_mines) break;
+
+                // If current index has a mine, skip to next index.
+                if (mineMap[i, j] == 9) continue;
+
                 // The mine is assigned when the random value is even number.
-                if (r.Next(1, 5) % 2 == 1) break;
+                if (r.Next(1, 5) % 2 == 1) continue;
 
                 mineMap[i, j] = 9;
                 count_assigned++;
 			}
 
-            if (i == (width - 1) && count_assigned != minesweeper) i = 0;
+            if (i == (width - 1) && count_assigned != count_mines) i = 0;
 		}
 
-        return array_map;
+        // Create map to find mines.
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                mineMap[i, j] = CalculateSurroundingMines(mineMap, i, j);
+            }
+        }
+
+        return mineMap;
     }
 
-    public void Map_block_open()
-	{
+    public void Map_block_open() {
 		prefeb_block_text.SetActive(true);
 	}
 }
