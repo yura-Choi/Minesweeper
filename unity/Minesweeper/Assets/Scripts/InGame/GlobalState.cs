c2bb320ba76ec35dfc434cd6b2d44a47556c401f
yura-Choi <dbfk1207@gmail.com>
2017-12-02 01:50:28 +0900

    Add script for management game state

---
+++ unity/Minesweeper/Assets/Scripts/InGame/GlobalState.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class GlobalState : MonoBehaviour {
+
+    private int width;
+    private int height;
+    private int landmine_count;
+    private int flag;
+
+    private void Start() {
+        this.flag = 0;
+    }
+
+    public void SettingMapInfo(int width, int height, int landmine) {
+        this.width = width;
+        this.height = height;
+        this.landmine_count = landmine;
+    }
+
+    public int GetWidth() {
+        return width;
+    }
+
+    public int GetHeight() {
+        return height;
+    }
+
+    public int GetLandMineLeft() {
+        return landmine_count - flag;
+    }
+
+    public int GetFlag() {
+        return flag;
+    }
+}
---
+++ unity/Minesweeper/Assets/Scripts/InGame/ManagePlayState.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagePlayState : MonoBehaviour {
+
+	public Text text_Flag;
+	public Text text_LandMine;
+
+	private GlobalState script_Global;
+
+	void Start () {
+        script_Global = GameObject.Find("InGameController").GetComponent<GlobalState>();
+	}
+	
+	void Update () {
+        text_LandMine.text = "LandMine  " + script_Global.GetLandMineLeft();
+        text_Flag.text = "Flag  " + script_Global.GetFlag();
+	}
+}
