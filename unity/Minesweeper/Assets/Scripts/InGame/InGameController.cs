33b809bc147d244c346e60c5118360e54fdc0f8a
yura-Choi <dbfk1207@gmail.com>
2017-12-02 01:57:33 +0900

    Change script directory construction

---
+++ unity/Minesweeper/Assets/Scripts/InGame/InGameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameController : MonoBehaviour {
+
+    public GameObject panel_SelectLevel;
+    public GameObject panel_GameScreen;
+
+	void Start () {
+        panel_SelectLevel.SetActive(true);
+        panel_GameScreen.SetActive(false);
+	}
+
+    // select level menu
+    public void level_EASY() {
+        PlayerPrefs.SetString("level", "easy");
+        panel_SelectLevel.SetActive(false);
+        panel_GameScreen.SetActive(true);
+	}
+	public void level_NORMAL() {
+		PlayerPrefs.SetString("level", "normal");
+        panel_SelectLevel.SetActive(false);
+        panel_GameScreen.SetActive(true);
+    }
+	public void level_HARD() {
+		PlayerPrefs.SetString("level", "hard");
+        panel_SelectLevel.SetActive(false);
+        panel_GameScreen.SetActive(true);
+    }
+
+    public void back_TITLE() {
+        SceneManager.LoadScene("Title");
+    }
+
+}
